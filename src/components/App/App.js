e725adaae6ca432c2f11073d1bb079e199ee0cd9
Chadwick Seth W <seth.chadwick@gmail.com>
2021-03-12 13:55:24 -0600

    components folder added with App, AllMeals, and MealDetail js and css files created

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Switch, Link, Route, Redirect } from 'react-router-dom'
+import './App.css';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Recipe App</h1>
+      </div>
+    );
+  }
+}
+
